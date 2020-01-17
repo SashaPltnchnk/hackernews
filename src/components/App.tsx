1f454449b307b6f3f7ab95b1e6e685b16014c58c
SashaPltnchnk <ZDubinaQ@gmail.com>
2020-01-17 16:21:48 +0200

    feat(components): connected header component to app, added routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
-import LinkList from './LinkList'
+import { Switch, Route } from 'react-router-dom'
+
+import Header from './Header'
+import LinkList from './LinkList';
+import CreateLink from './CreateLink'
 
 import './App.css'
 
 const App: React.FC = () => {
   return (
-    <LinkList />
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+        </Switch>
+      </div>
+    </div>
   );
 }
 
