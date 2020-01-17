c308949a793d909bcbf19b1cd7e53ee0feb11d37
SashaPltnchnk <ZDubinaQ@gmail.com>
2020-01-17 16:20:45 +0200

    feat(src): wrapped app with BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { BrowserRouter } from 'react-router-dom'
 
 
 const httpLink = createHttpLink({
@@ -20,9 +21,11 @@ const client = new ApolloClient({
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
